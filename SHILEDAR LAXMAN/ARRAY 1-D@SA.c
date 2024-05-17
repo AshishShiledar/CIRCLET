4787b149d97e52c307973faea9790a8114778335
AshishShiledar <168615363+AshishShiledar@users.noreply.github.com>
2024-05-17 12:43:23 +0530

    Add files via upload

---
+++ SHILEDAR LAXMAN/ARRAY 1-D@SA.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[5]={9,11,16,26,28};
+int i;
+for(i=0;i<5;i++)
+{
+printf("%d",a[3]);	
+	}	
+getch();	
+}
---
+++ SHILEDAR LAXMAN/SA8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,s,rows,j=0;
+printf("ENTER A NUMBER TO DEFINE THE ROWS: \n");
+scanf("%d",&rows);
+for(i=0;i<=rows;i++)
+{
+	for(s=0;s<=rows-1;s++)
+	{
+		printf(" ");
+	}
+	for(j=0;j<=(2*i-1);j++)
+	{
+		printf("%d ",i);
+	}
+	printf("\n");
+}
+getch();	
+}
